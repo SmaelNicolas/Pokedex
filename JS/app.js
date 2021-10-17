90184bb5f490ba7a59bfbb5cc423bb9c188fc92e
Smael Nicolas <nico.smael@gmail.com>
2021-10-16 23:25:02 -0300

    distribution , titles

---
+++ JS/app.js
@@ -0,0 +1,13 @@
+// let URLPOKEMON = "https://pokeapi.co/api/v2/pokemon/6";
+
+// console.log(URLPOKEMON);
+
+// $.get(URLPOKEMON, function (data) {
+//   console.log(data);
+//   console.log(data.sprites.other["official-artwork"].front_default);
+
+//   $("body").append(
+//     `<img src= ${data.sprites.other["official-artwork"].front_default}>
+//     <img src= ${data.sprites.front_shiny}>`
+//   );
+// });
