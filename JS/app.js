063bea1f46d7267aae8dc3e5751f5d148a26991f
Smael Nicolas <nico.smael@gmail.com>
2021-10-27 19:52:09 -0300

    variables

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,29 @@
-// let URLPOKEMON = "https://pokeapi.co/api/v2/pokemon/6";
+const URLPOKEMON = "https://pokeapi.co/api/v2/pokemon/1";
+
+let pokeNumber;
+let pokeImage;
+let pokeName;
+let pokeType;
+let pokeTypeSecond;
+let pokeCategory;
+let pokeAbilies;
+let pokeWeakness;
+let pokeShiny;
+let pokeHp;
+let pokeAttack;
+let pokeDefense;
+let pokeSpecialAtack;
+let pokeSpecialDefense;
+let pokeSpeed;
 
 // console.log(URLPOKEMON);
 
-// $.get(URLPOKEMON, function (data) {
-//   console.log(data);
-//   console.log(data.sprites.other["official-artwork"].front_default);
+$.get(URLPOKEMON, function (data) {
+  console.log(data);
+  //   console.log(data.sprites.other["official-artwork"].front_default);
 
-//   $("body").append(
-//     `<img src= ${data.sprites.other["official-artwork"].front_default}>
-//     <img src= ${data.sprites.front_shiny}>`
-//   );
-// });
+  //   $("body").append(
+  //     `<img src= ${data.sprites.other["official-artwork"].front_default}>
+  //     <img src= ${data.sprites.front_shiny}>`
+  //   );
+});
